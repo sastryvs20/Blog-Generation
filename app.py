e3452049b1b0e27fc3a9153b175b25bd61e96c50
Someswara Sastry Vadlamani <103941476+sastryvs20@users.noreply.github.com>
2024-02-26 21:56:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers, HuggingFaceHub
+import os
+
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+
+
+## Function To get response from LLAma 2 model
+def getLLamaresponse(input_text, no_words):
+
+    # LLama2 model (removed unnecessary parameter)
+    llm = HuggingFaceHub(repo_id='EleutherAI/gpt-neox-20b')
+
+    ## Prompt Template
+    template = """
+    Write a blog on the topic {input_text}
+    within {no_words} words. Make sure to use proper punctuations and grammar.
+    """
+
+    prompt = PromptTemplate(input_variables=["input_text", 'no_words'],
+                           template=template)
+
+    # Generate the response from the LLama 2 model
+    response = llm(prompt.format(input_text=input_text, no_words=no_words))
+    print(response)
+    return response
+
+
+# Set the Hugging Face API token from the environment variable (outside the function)
+
+st.set_page_config(page_title="Generate Blogs",
+                   page_icon='',
+                   layout='centered',
+                   initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs ")
+
+input_text = st.text_input("Blog Topic")
+
+## Creating two columns for additional fields
+col1, col2 = st.columns([5, 5])
+
+with col1:
+    no_words = st.text_input('No of Words')
+
+submit = st.button("Generate")
+
+## Final response
+if submit:
+    st.write(getLLamaresponse(input_text, no_words))
